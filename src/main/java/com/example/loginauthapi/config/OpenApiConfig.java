4d117f28ee44ac80e71690c8434c0264c7b2bd30
Kenji Higa Santos Lima <kenji.higa@tokiomarine.com.br>
2025-01-24 14:43:42 -0300

    Adicionando swagger

---
+++ src/main/java/com/example/loginauthapi/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.example.loginauthapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0.0")
+                        .description("Documentação da API do Projeto")
+                        .contact(new Contact()
+                                .name("Kenji Higa")
+                                .email("kekohiga@gmail.com")
+                                .url("https://podpah.com.br")));
+    }
+}
